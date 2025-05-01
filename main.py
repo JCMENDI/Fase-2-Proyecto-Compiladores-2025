ab82088a442e0e710967591947704c1df9f76a6a
José Carlos Mendizábal <115800136+JCMENDI@users.noreply.github.com>
2025-04-30 20:49:01 -0600

    Mejora de programa main con las llamadas a los archivos de ANTLR y Visitador.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from antlr4 import *
-
+from MiCompiladorLexer import MiCompiladorLexer
+from MiCompiladorParser import MiCompiladorParser
+from Visitador import EvalVisitor
 
 def main():
     print("Ingresa tu código (finaliza con una línea vacía):")
@@ -12,6 +14,13 @@ def main():
     
     input_stream = InputStream("\n".join(user_input))
 
+    Lexer = MiCompiladorLexer(input_stream)
+    stream = CommonTokenStream(Lexer)
+    parser = MiCompiladorParser(stream)
+
+    tree = parser.programa()  # Cambia 'programa' por el nombre de tu regla de inicio
+    visitor = EvalVisitor()
+    visitor.visit(tree)
 
 if __name__ == '__main__':
     main()
